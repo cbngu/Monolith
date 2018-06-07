3570ac9e27d4ff0517df8ff8c2b8ec4d052acbc6
Yannick <fishb6nes@gmail.com>
2018-06-07 22:33:45 +0100

    Export missing packages

--- monolith-api/src/main/java/module-info.java
+++ monolith-api/src/main/java/module-info.java
@@ -3,6 +3,22 @@ module gg.warcraft.monolith.api {
     requires transitive com.google.guice.extensions.assistedinject;
     requires transitive org.joml;
 
+    // command module
+    exports gg.warcraft.monolith.api.command;
+    exports gg.warcraft.monolith.api.command.event;
+    exports gg.warcraft.monolith.api.command.service;
+
+    // configuration module
+    exports gg.warcraft.monolith.api.config;
+
+    // core module
+    exports gg.warcraft.monolith.api.core;
+    exports gg.warcraft.monolith.api.core.event;
+
+    // effect module
+    exports gg.warcraft.monolith.api.effect;
+
+    // utility module
     exports gg.warcraft.monolith.api.util;
 
     // world module
