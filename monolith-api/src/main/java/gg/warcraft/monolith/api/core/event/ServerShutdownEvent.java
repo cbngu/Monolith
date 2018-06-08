606a794999338ab7aeb4b5a334eeec1abade21de
Yannick <fishb6nes@gmail.com>
2018-06-08 22:32:51 +0100

    Cleanup javadoc

--- monolith-api/src/main/java/gg/warcraft/monolith/api/core/event/ServerShutdownEvent.java
+++ monolith-api/src/main/java/gg/warcraft/monolith/api/core/event/ServerShutdownEvent.java
@@ -3,9 +3,10 @@ package gg.warcraft.monolith.api.core.event;
 import gg.warcraft.monolith.api.core.Event;
 
 /**
- * ServerShutdownEvent is a convenience event that allows subscribers to perform shutdown tasks without having to be
- * notified from the plugin's {@code onDisable} method. This event is fired synchronously and the server WILL shut down
- * this tick.
+ * ServerShutdownEvent is a quality of life event that allows subscribers to perform shutdown tasks without having to be
+ * notified from the plugin's disable method.
+ * <p>
+ * This event is fired synchronously and the server WILL shut down this tick.
  */
 public interface ServerShutdownEvent extends Event {
 
