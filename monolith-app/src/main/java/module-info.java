1afe69b90b3c9bb969a9ed418f0c384c02b3587c
Yannick <fishb6nes@gmail.com>
2018-06-09 22:07:42 +0100

    Break down module info by packages

--- monolith-app/src/main/java/module-info.java
+++ monolith-app/src/main/java/module-info.java
@@ -8,21 +8,48 @@ module gg.warcraft.monolith.app {
     requires github.api;
     requires jedis;
 
-    exports gg.warcraft.monolith.app.command to junit;
-    exports gg.warcraft.monolith.app.command.service to junit;
-    exports gg.warcraft.monolith.app.effect to junit;
-    exports gg.warcraft.monolith.app.effect.particle to junit;
-    exports gg.warcraft.monolith.app.effect.renderer to junit;
-    exports gg.warcraft.monolith.app.effect.vectors to junit;
-    exports gg.warcraft.monolith.app.util to junit;
-    exports gg.warcraft.monolith.app.world.block to junit;
+    exports gg.warcraft.monolith.app to gg.warcraft.monolith.spigot;
+
+    // command module
+    exports gg.warcraft.monolith.app.command to gg.warcraft.monolith.spigot, junit;
+    exports gg.warcraft.monolith.app.command.service to gg.warcraft.monolith.spigot, junit;
 
     opens gg.warcraft.monolith.app.command to org.mockito;
     opens gg.warcraft.monolith.app.command.service to org.mockito;
+
+    // effect module
+    exports gg.warcraft.monolith.app.effect to gg.warcraft.monolith.spigot, junit;
+    exports gg.warcraft.monolith.app.effect.particle to gg.warcraft.monolith.spigot, junit;
+    exports gg.warcraft.monolith.app.effect.renderer to gg.warcraft.monolith.spigot, junit;
+    exports gg.warcraft.monolith.app.effect.vectors to gg.warcraft.monolith.spigot, junit;
+
     opens gg.warcraft.monolith.app.effect to org.mockito;
     opens gg.warcraft.monolith.app.effect.particle to org.mockito;
     opens gg.warcraft.monolith.app.effect.renderer to org.mockito;
     opens gg.warcraft.monolith.app.effect.vectors to org.mockito;
+
+    // entity module
+    exports gg.warcraft.monolith.app.entity to gg.warcraft.monolith.spigot, junit;
+    exports gg.warcraft.monolith.app.entity.event to gg.warcraft.monolith.spigot, junit;
+    exports gg.warcraft.monolith.app.entity.service to gg.warcraft.monolith.spigot, junit;
+
+    exports gg.warcraft.monolith.app.entity.attribute to gg.warcraft.monolith.spigot, junit;
+    exports gg.warcraft.monolith.app.entity.attribute.service to gg.warcraft.monolith.spigot, junit;
+
+    exports gg.warcraft.monolith.app.entity.player to gg.warcraft.monolith.spigot, junit;
+    exports gg.warcraft.monolith.app.entity.player.event to gg.warcraft.monolith.spigot, junit;
+
+    exports gg.warcraft.monolith.app.entity.status to gg.warcraft.monolith.spigot, junit;
+    exports gg.warcraft.monolith.app.entity.status.service to gg.warcraft.monolith.spigot, junit;
+
+    // utility module
+    exports gg.warcraft.monolith.app.util to gg.warcraft.monolith.spigot, junit;
+
     opens gg.warcraft.monolith.app.util to org.mockito;
+
+    // world module
+    exports gg.warcraft.monolith.app.world to gg.warcraft.monolith.spigot, junit;
+    exports gg.warcraft.monolith.app.world.block to gg.warcraft.monolith.spigot, junit;
+
     opens gg.warcraft.monolith.app.world.block to org.mockito;
 }
