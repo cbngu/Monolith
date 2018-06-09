1f945e80483af50c700d43b14d274343d7da5b53
Yannick <fishb6nes@gmail.com>
2018-06-09 15:01:47 +0100

    Update Spigot command adapter to new API

--- monolith-spigot/src/main/java/gg/warcraft/monolith/spigot/command/SpigotCommandAdapter.java
+++ monolith-spigot/src/main/java/gg/warcraft/monolith/spigot/command/SpigotCommandAdapter.java
@@ -2,17 +2,14 @@ package gg.warcraft.monolith.spigot.command;
 
 import com.google.inject.Inject;
 import com.google.inject.Singleton;
-import gg.warcraft.monolith.api.command.Command;
 import gg.warcraft.monolith.api.command.service.CommandServerAdapter;
 import org.bukkit.Server;
 import org.bukkit.command.CommandMap;
-import org.bukkit.command.ConsoleCommandSender;
 import org.bukkit.command.PluginCommand;
-import org.bukkit.entity.Player;
 import org.bukkit.plugin.Plugin;
 
 import java.lang.reflect.Constructor;
-import java.lang.reflect.Field;
+import java.util.List;
 import java.util.UUID;
 
 @Singleton
@@ -32,7 +29,7 @@ public class SpigotCommandAdapter implements CommandServerAdapter {
         this.plugin = plugin;
 
         try {
-            final Field commandMapField = server.getClass().getDeclaredField("commandMap");
+            var commandMapField = server.getClass().getDeclaredField("commandMap");
             commandMapField.setAccessible(true);
             this.commandMap = (CommandMap) commandMapField.get(server);
         } catch (Exception ex) {
@@ -48,45 +45,44 @@ public class SpigotCommandAdapter implements CommandServerAdapter {
     }
 
     @Override
-    public boolean isCommandAvailable(String command) {
-        return server.getPluginCommand(command) == null;
+    public boolean isAliasAvailable(String alias) {
+        return server.getPluginCommand(alias) == null;
     }
 
     @Override
     public void dispatchCommandFor(String command, UUID playerId) {
-        Player player = server.getPlayer(playerId);
+        var player = server.getPlayer(playerId);
         player.performCommand(command);
     }
 
     @Override
     public void dispatchConsoleCommand(String command) {
-        ConsoleCommandSender consoleSender = server.getConsoleSender();
+        var consoleSender = server.getConsoleSender();
         server.dispatchCommand(consoleSender, command);
     }
 
     @Override
-    public void registerCommand(Command command) {
-        String name = command.getName();
-        PluginCommand pluginCommandByName = server.getPluginCommand(name);
+    public void registerCommand(String name, List<String> aliases) {
+        var pluginCommandByName = server.getPluginCommand(name);
         if (pluginCommandByName != null) {
-            String nameAlreadyExists = String.format(NAME_ALREADY_EXISTS, name);
+            var nameAlreadyExists = String.format(NAME_ALREADY_EXISTS, name);
             throw new IllegalArgumentException(nameAlreadyExists);
         }
 
-        command.getAliases().forEach(alias -> {
-            PluginCommand pluginCommandByAlias = server.getPluginCommand(alias);
+        aliases.forEach(alias -> {
+            var pluginCommandByAlias = server.getPluginCommand(alias);
             if (pluginCommandByAlias != null) {
-                String nameAlreadyExists = String.format(ALIAS_ALREADY_EXISTS, name, alias);
+                var nameAlreadyExists = String.format(ALIAS_ALREADY_EXISTS, name, alias);
                 throw new IllegalArgumentException(nameAlreadyExists);
             }
         });
 
         try {
-            PluginCommand newPluginCommand = pluginCommandConstructor.newInstance(name, plugin);
-            newPluginCommand.setAliases(command.getAliases());
+            var newPluginCommand = pluginCommandConstructor.newInstance(name, plugin);
+            newPluginCommand.setAliases(aliases);
             commandMap.register(name, newPluginCommand);
         } catch (Exception ex) {
-            String pluginCommandException = String.format("Failed to instantiate plugin command with name '%s'", name);
+            var pluginCommandException = String.format("Failed to instantiate plugin command with name '%s'", name);
             throw new IllegalStateException(pluginCommandException, ex.getCause());
         }
     }
