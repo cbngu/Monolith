7335a94857f67df134868b9eff1b30191aea5cd4
Yannick <fishb6nes@gmail.com>
2018-07-10 23:18:11 +0100

    Update mapper to map from Spigot to Monolith and include lore

--- monolith-spigot/src/main/java/gg/warcraft/monolith/spigot/item/SpigotItemMapper.java
+++ monolith-spigot/src/main/java/gg/warcraft/monolith/spigot/item/SpigotItemMapper.java
@@ -2,8 +2,11 @@ package gg.warcraft.monolith.spigot.item;
 
 import com.google.inject.Inject;
 import gg.warcraft.monolith.api.item.Item;
+import gg.warcraft.monolith.api.item.ItemType;
+import gg.warcraft.monolith.app.item.SimpleItem;
 import gg.warcraft.monolith.spigot.world.MaterialData;
 import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.ItemMeta;
 
 public class SpigotItemMapper {
     private final SpigotItemTypeMapper itemTypeMapper;
@@ -15,7 +18,17 @@ public class SpigotItemMapper {
 
     public ItemStack map(Item item) {
         MaterialData materialData = itemTypeMapper.map(item.getType());
-        return new ItemStack(materialData.getMaterial(), item.getStackSize(), (short) item.getDamage(),
+        ItemStack itemStack = new ItemStack(materialData.getMaterial(), item.getStackSize(), (short) item.getDamage(),
                 materialData.getData());
+        ItemMeta itemMeta = itemStack.getItemMeta();
+        itemMeta.setLore(item.getLore());
+        itemStack.setItemMeta(itemMeta);
+        return itemStack;
+    }
+
+    public Item map(ItemStack item) {
+        ItemType type = itemTypeMapper.map(item.getType(), item.getData().getData());
+        ItemMeta meta = item.getItemMeta();
+        return new SimpleItem(type, meta.getDisplayName(), item.getAmount(), item.getDurability(), meta.getLore());
     }
 }
