9f815068c782f092b22b6b1dc086f0f7d2d00f6b
Yannick <fishb6nes@gmail.com>
2018-09-12 19:36:22 +0100

    Delay menu adapter actions by 1 tick (safer per the Spigot API)

--- monolith-spigot/src/main/java/gg/warcraft/monolith/spigot/event/SpigotInventoryEventMapper.java
+++ monolith-spigot/src/main/java/gg/warcraft/monolith/spigot/event/SpigotInventoryEventMapper.java
@@ -1,33 +1,25 @@
 package gg.warcraft.monolith.spigot.event;
 
 import com.google.inject.Inject;
-import gg.warcraft.monolith.api.core.EventService;
 import gg.warcraft.monolith.api.menu.Button;
 import gg.warcraft.monolith.api.menu.Click;
 import gg.warcraft.monolith.api.menu.Menu;
 import gg.warcraft.monolith.api.menu.service.MenuQueryService;
-import gg.warcraft.monolith.api.menu.service.MenuRepository;
 import gg.warcraft.monolith.app.menu.SimpleClick;
 import gg.warcraft.monolith.spigot.menu.MonolithMenuHolder;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
 import org.bukkit.event.inventory.InventoryClickEvent;
-import org.bukkit.event.inventory.InventoryCloseEvent;
 
 import java.util.UUID;
 
 public class SpigotInventoryEventMapper implements Listener {
-    private final EventService eventService;
     private final MenuQueryService menuQueryService;
-    private final MenuRepository menuRepository;
 
     @Inject
-    public SpigotInventoryEventMapper(EventService eventService, MenuQueryService menuQueryService,
-                                      MenuRepository menuRepository) {
-        this.eventService = eventService;
+    public SpigotInventoryEventMapper(MenuQueryService menuQueryService) {
         this.menuQueryService = menuQueryService;
-        this.menuRepository = menuRepository;
     }
 
     @EventHandler(priority = EventPriority.HIGH)
@@ -49,18 +41,6 @@ public class SpigotInventoryEventMapper implements Listener {
             }
         } else {
             // TODO publish Monolith inventory pre click event
-        }
-    }
-
-    @EventHandler(priority = EventPriority.HIGH)
-    public void onInventoryCloseEvent(InventoryCloseEvent event) {
-        UUID clickerId = event.getPlayer().getUniqueId();
-        Menu menu = menuQueryService.getMenu(clickerId);
-        if (menu != null) {
-            menuRepository.delete(clickerId);
-            // TODO publish Monolith menu click event
-        } else {
-            // TODO publish Monolith inventory close event
         }
     }
 }
--- monolith-spigot/src/main/java/gg/warcraft/monolith/spigot/menu/service/SpigotMenuAdapter.java
+++ monolith-spigot/src/main/java/gg/warcraft/monolith/spigot/menu/service/SpigotMenuAdapter.java
@@ -1,6 +1,7 @@
 package gg.warcraft.monolith.spigot.menu.service;
 
 import com.google.inject.Inject;
+import gg.warcraft.monolith.api.core.TaskService;
 import gg.warcraft.monolith.api.menu.Menu;
 import gg.warcraft.monolith.api.menu.service.MenuServerAdapter;
 import gg.warcraft.monolith.spigot.menu.SpigotMenuMapper;
@@ -13,23 +14,25 @@ import java.util.UUID;
 public class SpigotMenuAdapter implements MenuServerAdapter {
     private final Server server;
     private final SpigotMenuMapper menuMapper;
+    private final TaskService taskService;
 
     @Inject
-    public SpigotMenuAdapter(Server server, SpigotMenuMapper menuMapper) {
+    public SpigotMenuAdapter(Server server, SpigotMenuMapper menuMapper, TaskService taskService) {
         this.server = server;
         this.menuMapper = menuMapper;
+        this.taskService = taskService;
     }
 
     @Override
     public void showMenu(Menu menu, UUID viewerId) {
         Inventory inventory = menuMapper.map(menu, viewerId);
         Player player = server.getPlayer(viewerId);
-        player.openInventory(inventory);
+        taskService.runNextTick(() -> player.openInventory(inventory));
     }
 
     @Override
     public void closeMenu(UUID viewerId) {
         Player player = server.getPlayer(viewerId);
-        player.closeInventory();
+        taskService.runNextTick(player::closeInventory);
     }
 }
