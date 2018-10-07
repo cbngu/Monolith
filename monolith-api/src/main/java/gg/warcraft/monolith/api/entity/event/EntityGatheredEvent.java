0eb9246b691e100c1fda5835559fa712723cd89c
cbngu <chaubnguyen123@gmail.com>
2018-10-07 14:39:50 +0100

    Revert "[GATHERING-1] added simple entity/block gathered events"
    This reverts commit 79cc42042da2868879e210fb260ce1e659a2bf41.

--- monolith-api/src/main/java/gg/warcraft/monolith/api/entity/event/EntityGatheredEvent.java
+++ monolith-api/src/main/java/gg/warcraft/monolith/api/entity/event/EntityGatheredEvent.java
@@ -1,4 +1,12 @@
 package gg.warcraft.monolith.api.entity.event;
 
+import java.util.UUID;
+
+import gg.warcraft.monolith.api.entity.Entity;
+
 public interface EntityGatheredEvent {
+
+    Entity getGatheredEntity();
+
+    UUID getPlayerId();
 }
