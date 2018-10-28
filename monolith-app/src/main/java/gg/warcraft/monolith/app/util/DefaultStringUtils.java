24e36e0733385d56dedf68d298b766252b369a01
Yannick <fishb6nes@gmail.com>
2018-10-28 08:33:27 +0100

    Move removeChatCodes utility function to StringUtils

--- monolith-app/src/main/java/gg/warcraft/monolith/app/util/DefaultStringUtils.java
+++ monolith-app/src/main/java/gg/warcraft/monolith/app/util/DefaultStringUtils.java
@@ -2,9 +2,16 @@ package gg.warcraft.monolith.app.util;
 
 import gg.warcraft.monolith.api.util.StringUtils;
 
+import java.util.regex.Pattern;
+
 public class DefaultStringUtils implements StringUtils {
+    private static final Pattern chatCodePattern = Pattern.compile("§.");
 
     public boolean isVowel(char c) {
         return "aeiouAEIOU".indexOf(c) >= 0;
+    }
+
+    public String removeChatCodes(String string) {
+        return chatCodePattern.matcher(string).replaceAll("");
     }
 }
