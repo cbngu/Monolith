c8d12405f80ab643f2dbde038608c236dc4443b6
Yannick <fishb6nes@gmail.com>
2018-11-04 21:39:13 +0100

    Force Guava EventBus to dispatch events immediately

--- monolith-app/src/main/java/gg/warcraft/monolith/app/core/GuavaEventService.java
+++ monolith-app/src/main/java/gg/warcraft/monolith/app/core/GuavaEventService.java
@@ -5,17 +5,29 @@ import com.google.inject.Singleton;
 import gg.warcraft.monolith.api.core.Event;
 import gg.warcraft.monolith.api.core.EventService;
 
+import java.lang.reflect.Field;
+import java.lang.reflect.Method;
+
 @Singleton
 public class GuavaEventService implements EventService {
     private final EventBus bus;
 
     public GuavaEventService() {
         this.bus = new EventBus();
+        try {
+            Field dispatcherField = EventBus.class.getDeclaredField("dispatcher");
+            dispatcherField.setAccessible(true);
+            Class<?> dispatcherClass = dispatcherField.get(this.bus).getClass().getSuperclass();
+            Method immediateDispatcher = dispatcherClass.getDeclaredMethod("immediate");
+            immediateDispatcher.setAccessible(true);
+            dispatcherField.set(this.bus, immediateDispatcher.invoke(null));
+        } catch (Exception ex) {
+            throw new IllegalStateException("Failed to initialize event service dispatcher: " + ex.getMessage());
+        }
     }
 
     @Override
     public void publish(Event event) {
-        System.out.println("DEBUG Event service asked to publish: " + event.getClass().getSimpleName());
         bus.post(event);
     }
 
