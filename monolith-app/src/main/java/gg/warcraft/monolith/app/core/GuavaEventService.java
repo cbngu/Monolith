c758ec5685372870be574527bd52be275421076c
Yannick <fishb6nes@gmail.com>
2018-11-05 10:45:19 +0100

    Simplify synchronous event bus initialization

--- monolith-app/src/main/java/gg/warcraft/monolith/app/core/GuavaEventService.java
+++ monolith-app/src/main/java/gg/warcraft/monolith/app/core/GuavaEventService.java
@@ -1,29 +1,18 @@
 package gg.warcraft.monolith.app.core;
 
+import com.google.common.eventbus.AsyncEventBus;
 import com.google.common.eventbus.EventBus;
+import com.google.common.util.concurrent.MoreExecutors;
 import com.google.inject.Singleton;
 import gg.warcraft.monolith.api.core.Event;
 import gg.warcraft.monolith.api.core.EventService;
-
-import java.lang.reflect.Field;
-import java.lang.reflect.Method;
 
 @Singleton
 public class GuavaEventService implements EventService {
     private final EventBus bus;
 
     public GuavaEventService() {
-        this.bus = new EventBus();
-        try {
-            Field dispatcherField = EventBus.class.getDeclaredField("dispatcher");
-            dispatcherField.setAccessible(true);
-            Class<?> dispatcherClass = dispatcherField.get(this.bus).getClass().getSuperclass();
-            Method immediateDispatcher = dispatcherClass.getDeclaredMethod("immediate");
-            immediateDispatcher.setAccessible(true);
-            dispatcherField.set(this.bus, immediateDispatcher.invoke(null));
-        } catch (Exception ex) {
-            throw new IllegalStateException("Failed to initialize event service dispatcher: " + ex.getMessage());
-        }
+        this.bus = new AsyncEventBus(MoreExecutors.newDirectExecutorService());
     }
 
     @Override
